c49ecb50f692cf2916c01f10f7512f7891c51bc6
Gem <baos2019731@gmail.com>
2024-10-06 17:24:29 -0700

    created novel class

---
+++ codes/Novel.java
@@ -0,0 +1,40 @@
+
+
+public class Novel
+{
+
+    private String title;
+    private String authorName;
+    private int yearPublished;
+
+
+    public Novel(String title, String authorName, int yearPublished)
+    {
+        this.title = title;
+        this.authorName = authorName;
+        this.yearPublished = yearPublished;
+    }
+
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthorName()
+    {
+        return authorName;
+    }
+
+    public int getYearPublished()
+    {
+        return yearPublished;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return title + " by " + authorName + " (" + yearPublished + ")";
+    }
+}
