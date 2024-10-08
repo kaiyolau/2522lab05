326bf24aabd14786b4034a6091ef569ec229a733
Yuho Lim <dbgh7894@gmail.com>
2024-10-07 19:37:27 -0700

    create 5 functions: howManyBooksContain to getBooksThisLength and some main

---
+++ codes/ca/bcit/comp2522/lab5/BookStore.java
@@ -0,0 +1,38 @@
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BookStore
+{
+    private final List<Novel> list;
+    BookStore()
+    {
+
+    }
+
+    public howMantBooksContain(final String word)
+    {
+
+    }
+
+    public whichPercentWrittenBetween(final int first,
+                                      final int last)
+    {
+
+    }
+
+    public getOldestBook(){
+        return;
+    }
+
+    public getBooksThisLength(final int titleLength)
+    {
+
+    }
+
+    public main(final String[] args)
+    {
+
+    }
+}
